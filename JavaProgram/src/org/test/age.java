f3f101cc5a09a8c46e1f3fc3a0c4d85a40c6fd7a
Anitha <Anitha@192.168.100.4>
2019-08-17 22:53:39 +0530

    task4

---
+++ JavaProgram/src/org/test/age.java
@@ -0,0 +1,21 @@
+package org.test;
+
+public class age {
+	public static void main(String []args) {
+		int age[]=new int[5];
+		
+			age[0]=25;
+			age[1]=20;
+			age[2]=23;
+			age[3]=24;
+			age[4]=26;
+			for(int i=0;i<age.length;i++)
+			{
+				System.out.println(age[i]);
+			}
+	}
+}
+	
+	
+		
+	
